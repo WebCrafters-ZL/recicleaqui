bb0694416e2b7ea0a32287f83fd034fd90aebcf7
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2024-03-26 17:11:03 -0300

    feat: Comentários
    Comentários no código para facilitar a compreensão

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
+// Importação do módulo 'express' para criação de um servidor web
 const express = require("express");
+
+// Criação de um novo roteador usando o método Router() do Express
 const router = express.Router();
 
-router.get("/", function(req, res){
+// Definição da rota para o caminho raiz ('/') usando o método GET
+router.get("/", function (req, res) {
+    // Renderização do template 'home' com os dados passados ({ title: "Demo do Bootstrap" })
     res.render("home", { title: "Demo do Bootstrap" });
 })
 
+// Exportação do roteador para uso em outros arquivos
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,14 @@
+// Importação do módulo 'express' para criação de um servidor web
 const express = require("express");
+
+// Criação de um novo roteador usando o método Router() do Express
 const router = express.Router();
 
+// Definição da rota para o caminho raiz ('/') usando o método GET
 router.get("/", function (req, res) {
+    // Renderização do template 'login' com os dados passados ({ title: "RecicleAqui - Login" })
     res.render("login", { title: "RecicleAqui - Login" });
 })
 
+// Exportação do roteador para uso em outros arquivos
 module.exports = router;
