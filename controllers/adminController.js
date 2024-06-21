1cdcdc6bd9e6e73f4f9ee2ee9bc70a7fe9fe251d
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-06-21 01:51:47 -0300

    att, rota para consulta de agendamentos admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,22 +2,46 @@ const db = require("../db/models");
 
 const adminView = async function (req, res, next) {
     try {
-        // Renderiza a view 'adminView' passando os o título da página como parâmetro
-        res.render("adminView", { title: "Recicle Aqui - Administração" })
+        // Renderiza a view 'adminView' passando o título da página como parâmetro
+        res.render("adminView", { title: "Recicle Aqui - Administração" });
     } catch (error) {
         next(error); // Passa o erro para o próximo middleware de erro
     }
 }
 
-const usuariosView = async function (req, res, next) {
+const clientesView = async function (req, res, next) {
     try {
-        res.render("usuariosView", { title: "Gerenciamento de Usuários" }); // Renderiza a view 'usuariosView'
+        const clients = await db.Cliente.findAll({
+            include: {
+                model: db.Usuario
+            }
+        });
+        res.render("clientesView", { title: "Lista de Clientes", clients });
     } catch (error) {
         next(error);
     }
 };
 
+const usuariosView = async function (req, res, next) {
+    try {
+        res.render("usuariosView", { title: "Gerenciamento de Usuários" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const agendamentosView = async function (req, res) {
+    try {
+        res.render("agendamentosView", { title: "Lista de Agendamentos"});
+    } catch (error) {
+        console.error('Erro ao renderizar a página de agendamentos:', error);
+        res.status(500).send('Erro ao carregar a página de agendamentos');
+    }
+};
+
 module.exports = {
     adminView,
-    usuariosView
-}
+    usuariosView,
+    clientesView,
+    agendamentosView
+};
--- routes/admin.js
+++ routes/admin.js
@@ -9,17 +9,19 @@ const router = express.Router();
 // Importa as funções do controlador adminController
 const { 
     adminView,
-    usuariosView
+    usuariosView,
+    agendamentosView
  } = require("../controllers/adminController");
 
 // Importa a função requerirLogin do middleware requerirLogin
 const requerirLogin = require("../middleware/requerirLogin");
-
 // Definição da rota para o caminho '/' usando o método GET e associa a função adminView
 router.get("/", requerirLogin, adminView);
-
 // Rota para a página de gerenciamento de usuários
 router.get("/usuarios", requerirLogin, usuariosView);
+// Rota para a página de gerenciamento de Agendamentos
+router.get("/agendamentos", requerirLogin, agendamentosView);
+
 
 // Exportação do roteador para uso em outros arquivos
 module.exports = router;
